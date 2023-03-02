1bc112ce0e5d17d8785fffb53cd4528507eece9d
Nafiz132 <nafizurrahman132@gmail.com>
2023-03-03 02:23:58 +0600

    simple laravel blog

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pt-2">
+                 <div class="row">
+                    <div class="col-8">
+                        <h1 class="display-one">Have a look on our blogs</h1>
+                        <p>Read these blogs to find the amazing natural beauties of Bangladesh and the hospitality of this country's people</p>
+                    </div>
+                    <div class="col-4">
+                        <p>Create new Post</p>
+                        <a href="/blog/create/post" class="btn btn-primary btn-sm">Add Post</a>
+                    </div>
+                </div>                
+                @forelse($posts as $post)
+                    <ul>
+                        <li><a href="./blog/{{ $post->id }}">{{ ucfirst($post->title) }}</a></li>
+                    </ul>
+                @empty
+                    <p class="text-warning">No blog Posts available</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title text-align=center>Travel Blog</title>
+    <!-- <title>{{ config('app.name') }}</title>-->
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+            body {
+                font-family: 'Arial';
+            }
+        </style>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
